dbe86beb3056a5982a02c2fe88e6be7e4502d345
solomonkifle <102066454+solomonkifle@users.noreply.github.com>
2022-07-26 20:26:23 +0300

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("Holberton, " * (magic_string.n - 1) + "Holberton")
+def magic_string(static={"count": 0}):
+    static["count"] += 1
+    return str("BestSchool, " * static["count"])[:-2]
